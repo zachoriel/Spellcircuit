7e9f5505bbe7e7670f8c377a8e52e77f42fc6659
Zach Oriel <zachoriel96@gmail.com>
2025-05-24 00:36:51 -0700

    Added player animations

---
+++ Assets/Scripts/Misc/Globals.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public static class Globals
+{
+    public struct KeyBinds
+    {
+        public static KeyCode RUN_KEY = KeyCode.LeftShift;
+        public static KeyCode JUMP_KEY = KeyCode.Space;
+    }
+
+    public struct Tags
+    {
+        public static string PLAYER_TAG = "player";
+    }
+}
--- Assets/Scripts/Player/Controls/ThirdPersonMovement.cs
+++ Assets/Scripts/Player/Controls/ThirdPersonMovement.cs
@@ -5,20 +5,34 @@ public class ThirdPersonMovement : MonoBehaviour
     [Header("Components")]
     [SerializeField] private CharacterController controller;
     [SerializeField] private Transform cam;
+    [SerializeField] private Animator animator;
 
     [Header("Settings")]
-    [SerializeField] private float speed = 6f;
+    [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float runSpeed = 6f;
     [SerializeField] private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
 
-    void Update()
+    private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        bool running = Input.GetKey(Globals.KeyBinds.RUN_KEY);
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        float speed = 0f;
+
         if (direction.magnitude >= 0.1f)
         {
+            speed = running ? runSpeed : walkSpeed;
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -26,5 +40,9 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        float speedPercent = direction.magnitude * (running ? 1f : 0.5f);
+        animator.SetFloat("Speed", speedPercent);
+        animator.SetFloat("Horizontal", horizontal);
     }
 }
