041a32d56f012246d75927a272b9c1c7c7bfc957
Zach Oriel <zachoriel96@gmail.com>
2025-05-24 15:39:56 -0700

    Spell test fire

--- Assets/Scripts/Player/SpellCaster.cs
+++ Assets/Scripts/Player/SpellCaster.cs
@@ -1,7 +1,50 @@
+using System.Collections;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class SpellCaster : MonoBehaviour
 {
+    [SerializeField] private GameObject spellPrefab;
+    [SerializeField] private Transform spellSpawnPoint;
+    [SerializeField] private Camera cam;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CastTestSpell();
+        }
+    }
+
+    private void CastTestSpell()
+    {
+        Vector3 direction;
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            direction = (hit.point - spellSpawnPoint.position).normalized;
+        }
+        else
+        {
+            direction = cam.transform.forward;
+        }
+
+        GameObject newSpell = Instantiate(spellPrefab, spellSpawnPoint.position, Quaternion.LookRotation(direction));
+
+        var motion = newSpell.GetComponentInChildren<RFX1_TransformMotion>();
+        if (motion != null)
+        {
+            motion.CollisionEnter += (sender, collisionInfo) =>
+            {
+                if (collisionInfo.Hit.collider.TryGetComponent(out Enemy enemy))
+                {
+                    enemy.TakeDamage(5f);
+                }
+            };
+        }
+    }
+
     public void CastSpell(Spell spell)
     {
         // Here I can instantiate a projectile prefab based on spell.shape
